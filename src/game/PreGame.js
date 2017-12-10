ee67ba8d7d412049d6ec7b6339dea1f4acd304b4
Zac-L <Zachlichens@gmail.com>
2017-12-10 09:41:52 -0800

    basic build out of form element for possible pregame logic

--- src/game/PreGame.js
+++ src/game/PreGame.js
@@ -6,6 +6,32 @@ class PreGame extends PureComponent {
     return (
       <main>
         <h3>PreGame component</h3>
+        <form>
+         <fieldset>
+           <legend>Variate(using this name for now) Selection</legend>
+            <select>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+         </fieldset>
+          <legend>Difficulty Selection</legend>
+         <fieldset>
+           <div>
+           <input type="radio" name="difficulty" id="difficulty_easy" value="easy"/>
+           <label for="difficulty_easy">Easy</label>
+           </div>
+           <div>
+           <input type="radio" name="difficulty" id="difficulty_medium" value="medium"/>
+           <label for="difficulty_medium">Medium</label>
+           </div>
+           <div>
+           <input type="radio" name="difficulty" id="difficulty_hard" value="hard"/>
+           <label for="difficulty_hard">Hard</label>
+           </div>
+         </fieldset>
+        </form>
       </main>
     );
   }
