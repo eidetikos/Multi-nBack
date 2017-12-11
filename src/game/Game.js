83c8b9c34e615c5b398c909d8bad736a94334936
Zac-L <Zachlichens@gmail.com>
2017-12-11 11:21:22 -0800

    removed unneeded constructor

--- src/game/Game.js
+++ src/game/Game.js
@@ -1,5 +1,4 @@
 import React, { PureComponent } from 'react';
-import { Switch, Route } from 'react-router';
 import { connect } from 'react-redux';
 
 import PreGame from './PreGame';
@@ -7,53 +6,20 @@ import InGame from './InGame';
 import PostGame from './PostGame';
 
 class Game extends PureComponent {
-  
-  // set up fake data
-  constructor() {
-    super();
-    this.state = {
-      difficulty: {
-        initN: 2,
-        interval: 1000
-      },
-      numVariates: 4,
-      status: 'post',
-      sequences: [{
-        variates: {
-          position: true,
-          number: true,
-          audio: false,
-          shape: true,
-          color: true
-        },
-        nBack: 3,
-        interval: 1000,
-        combos: [{
-          position: 6,
-          number: 3,
-          shape: 'square',
-          color: 'red'
-        }],
-        comboPointer: 0
-      }],
-      finalStats: []
-    };
-  }
 
   render() {
-    console.log('in Game', this.state)
-    const { match: { url } } = this.props;
+    const { match: { url }, game } = this.props;
     return (
       <main className="game">
         <h2>Game component</h2>
-        {this.state.status === 'pre' &&
-          <PreGame tempGame={this.state}/>
+        {game.status === 'pre' &&
+          <PreGame tempGame={game}/>
         }
-        {this.state.status === 'in' &&
-          <InGame tempGame={this.state}/>
+        {game.status === 'in' &&
+          <InGame tempGame={game}/>
         }
-        {this.state.status === 'post' &&
-          <PostGame tempGame={this.state}/>
+        {game.status === 'post' &&
+          <PostGame tempGame={game}/>
         }
       </main>
     );
@@ -61,6 +27,6 @@ class Game extends PureComponent {
 }
 
 export default connect(
-  state => ({}),
+  state => ({ game: state.game }),
   null
 )(Game);
