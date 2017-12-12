274dcd57c7be0cd930f17a399b855c1a9063bbe9
ckrountree <jmail0207@gmail.com>
2017-12-12 12:47:24 -0800

    create Auth api, actions and reducer

--- src/auth/reducer.js
+++ src/auth/reducer.js
@@ -1,5 +1,11 @@
+import * as actions from '../constants';
+
 export function user(store = null, { type, payload }) {
   switch (type) {
+    case actions.SIGN_IN:
+      return payload;
+    case actions.LOG_OUT:
+      return null;
     default:
       return store;
   }
