abea2d70045e4d41d400ae0949effd04be69225f
ckrountree <jmail0207@gmail.com>
2017-12-12 14:38:46 -0800

    start about

--- src/about/About.js
+++ src/about/About.js
@@ -5,7 +5,8 @@ class About extends PureComponent {
   render() {
     return (
       <main className="about">
-        <h2>about component</h2>
+        <h2>About component</h2>
+        <p>It has been thought that working memory (WM), a cognitive process, and IQ correlate, perhaps using the same neural networks.  WM is a "trainable" process.</p>
       </main>
     );
   }
