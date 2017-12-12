03833f0e2115bd7de73b0efea48b6b8fbea56022
ckrountree <jmail0207@gmail.com>
2017-12-12 12:45:58 -0800

    add auth actions to constants

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+// ###  Auth Constants ### //
+export const SIGN_IN = 'SIGN_IN';
+export const LOG_OUT = 'LOG_OUT';
+
+// ###  Game Constants ### //
+export const INIT_GAME = 'INIT_GAME';
+export const GET_SETTINGS = 'GET_SETTINGS';
+export const INIT_SEQUENCE = 'INIT_SEQUENCE';
+export const NEXT_COMBO = 'NEXT_COMBO';
+export const SEQUENCE_OVER = 'SEQUENCE_OVER';
+export const GAME_OVER = 'GAME_OVER';
+export const LEAVE_GAME = 'LEAVE_GAME';
