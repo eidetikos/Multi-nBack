5314cf3cf52739340821ef2190c2153940884324
Zac-L <Zachlichens@gmail.com>
2017-12-12 12:20:27 -0800

    add constatns for possible loading and error displays

---
+++ src/app/constants.js
@@ -0,0 +1,14 @@
+// ###  Game Constants ### //
+export const INIT_GAME = 'INIT_GAME';
+export const SET_SETTINGS = 'SET_SETTINGS';
+export const INIT_SEQUENCE = 'INIT_SEQUENCE';
+export const NEXT_COMBO = 'NEXT_COMBO';
+export const SEQUENCE_OVER = 'SEQUENCE_OVER';
+export const GAME_OVER = 'GAME_OVER';
+export const WRAP_UP = 'WRAP_UP';
+export const LEAVE_GAME = 'LEAVE_GAME';
+
+// ### Loading Constants ### //
+export const LOADING = 'LOADING';
+export const LOADED = 'LOADED';
+export const ERROR = 'ERROR';
