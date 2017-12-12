b38d8ed2e1f4ca50f9e2dd537b486cd43e796271
Zac-L <Zachlichens@gmail.com>
2017-12-12 12:43:57 -0800

    temp token to work on game api

--- src/services/api.js
+++ src/services/api.js
@@ -3,12 +3,12 @@ const url = process.env.REACT_APP_API_URL;
 const wrap = promise => {
   return promise.then(response => {
     console.log(response);
-    response.json().then(res => console.log(res));
+    // response.json().then(res => console.log(res));
     if(!response.ok) return Promise.reject(new Error(response.statusText));
-    return response.json();
+    return response.json().then(res => console.log(res));
   });
 };
-
+const token =  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjVhMzAzMzdhOGVlZGNjOTk4NDJiNjBlOCIsImlhdCI6MTUxMzEwODM0Nn0.t_RbtzpaS83KuSv7Z-Y61x4QT2VZPuV6WeKa1GU7hC0';
 export const get = path => wrap(fetch(`${url}${path}`));
 
 export const post = (path, data) => wrap(
@@ -16,6 +16,7 @@ export const post = (path, data) => wrap(
     method: 'post',
     body: JSON.stringify(data),
     headers: {
+      'Authorization': token,
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     }
