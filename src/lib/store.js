802b1a5d082044a5a61144e5a15078e2a1edee9c
Zac-L <Zachlichens@gmail.com>
2017-12-12 12:21:48 -0800

    added promise middleware

---
+++ src/lib/store.js
@@ -0,0 +1,22 @@
+import { createStore, compose, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import promiseMiddleware from './promise-middleware';
+import { user } from '../auth/reducer';
+import  game  from '../game/reducer/game.reducer';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+  user, game
+});
+
+export const store = createStore(
+  rootReducer,
+  composeEnhancers(
+    applyMiddleware(
+      thunk,
+      promiseMiddleware
+    )
+  )
+);
+
