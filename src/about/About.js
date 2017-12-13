1a6a3547e8cf53830e61f0b41c0b83a48372ecd4
ckrountree <jmail0207@gmail.com>
2017-12-12 19:20:30 -0800

    committing to pull down from  master

--- src/about/About.js
+++ src/about/About.js
@@ -6,8 +6,10 @@ class About extends PureComponent {
     return (
       <main className="about">
         <h2>About component</h2>
-        <p>It has been thought that working memory (WM), a cognitive process, and IQ correlate, perhaps using the same neural networks.  
-          WM is a "trainable" process.  Therefore, the hope is that, by improving WM, IQ can also be improved.</p>
+        <p>Working memory (WM) is a cognitive process.  WM is thought to be "trainable", relating to how much a person can think about at one time.  
+        It is thought that WM and IQ correlate, perhaps using the same neural networks.  Therefore, the hope is that, by improving WM, IQ will also be improved.</p>
+        <p>There are many "mind" games on the market, from Sudoku to crossword puzzles, to memory-matching games.  Studies have shown that none of these have demonstrated 
+        any marked improvement in memory...until the emergence of the Dual-N-Back memory game which calls upon the user to recall multiple things instead of just one.</p>
       </main>
     );
   }
