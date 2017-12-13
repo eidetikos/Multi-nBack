ed5fbb1a89462d726c90fe8cfc314abd6bbe0927
ckrountree <jmail0207@gmail.com>
2017-12-13 10:54:13 -0800

    creating user stats page

--- src/game/PostGame.js
+++ src/game/PostGame.js
@@ -4,9 +4,25 @@ import { connect } from 'react-redux';
 class PostGame extends PureComponent {
   render() {
     const finalStats = this.props.game.finalStats;
+    const user = this.props.user;
     return (
       <div className="post-game">
-        <h3>PostGame component</h3>
+        <h1>{user}'s Final Stats</h1>
+        <div className="leaderboard">
+          <h3>LEADERBOARD: {}</h3>
+        </div>
+        <div className="nBack-Chart"></div>
+        <div>
+          <h3>Game Stats</h3>
+          <ul>
+            <li>
+              Your highest N achieved: { finalStats.highN }
+            </li>
+            <li>
+              Your average N achieved: { finalStats.avgN }
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
@@ -15,6 +31,7 @@ class PostGame extends PureComponent {
 export default connect(
   state => ({
     game: state.game,
+    user: state.user
   }),
   null
 )(PostGame);
