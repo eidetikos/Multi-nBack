8faf21bc8db42b20df2d0e968ab6b913f5dc6b87
Zac-L <Zachlichens@gmail.com>
2017-12-13 09:41:36 -0800

    added try / catch for auth actions to display error and loading messages

--- src/auth/actions.js
+++ src/auth/actions.js
@@ -1,15 +1,33 @@
 import * as auth from '../services/authApi';
+import * as actions from '../app/constants';
 
 export function signUp(name, password) {
   return async dispatch => {
-    const token = await auth.post('/signup', { name, password });
-    localStorage.setItem('token', token.token);
+    try {
+      const token = await auth.post('/signup', { name, password });
+      localStorage.setItem('token', token.token);
+    }
+    catch(error) {
+      dispatch({
+        type: actions.ERROR,
+        payload: error
+      });
+    }
   };
 }
 
 export function signIn(name, password) {
   return async dispatch => {
-    const token = await auth.post('/signin', { name, password });
-    localStorage.setItem('token', token.token);
+    
+    try {
+      const token = await auth.post('/signin', { name, password });
+      localStorage.setItem('token', token.token);
+    }
+    catch(error) {
+      dispatch({
+        type: actions.ERROR,
+        payload: error
+      });
+    }
   };
 }
