e594a8cfd98d8ad5d15cee49c19af1468ddab102
ckrountree <jmail0207@gmail.com>
2017-12-14 10:06:36 -0800

    add back Chart component to PostGame that was never merged

--- src/game/PostGame.js
+++ src/game/PostGame.js
@@ -5,27 +5,31 @@ import Chart from './Chart';
 class PostGame extends PureComponent {
   render() {
     const finalStats = this.props.game.finalStats;
-    const user = this.props.user;
-    return (
-      <div className="post-game">
-        {/* <h1>{user}'s Final Stats</h1> */}
-        <div className="leaderboard">
-          <h3>LEADERBOARD: {}</h3>
+    return (finalStats.game ?
+      (
+        <div className="post-game">
+          <h1>{finalStats.user.name}'s Final Stats</h1>
+          <div className="leaderboard">
+            <h3>LEADERBOARD: {}</h3>
+          </div>
+          <div className="nBack-Chart">
+            <Chart/>
+          </div>
+          <div>
+            <h3>Game Stats</h3>
+            <ul>
+              <li>
+              Your highest N achieved: { finalStats.game.highN }
+              </li>
+              <li>
+              Your average N achieved: { finalStats.game.avgN }
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="nBack-Chart"></div>
-        {/* <div>
-          <h3>Game Stats</h3>
-          <ul>
-            <li>
-              Your highest N achieved: { finalStats.highN }
-            </li>
-            <li>
-              Your average N achieved: { finalStats.avgN }
-            </li>
-          </ul>
-        </div> */}
-      </div>
-    );
+      ) : (
+        <div className="post-game"></div>
+      ));
   }
 }
 
