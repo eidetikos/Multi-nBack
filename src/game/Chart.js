95fb4830cc3376e5075488e8de5f9cee41beae1d
ckrountree <jmail0207@gmail.com>
2017-12-13 17:49:45 -0800

    create working chart showing our nBack data

--- src/game/Chart.js
+++ src/game/Chart.js
@@ -3,12 +3,48 @@ import { connect } from 'react-redux';
 import RC2 from 'react-chartjs2';
 
 export class Chart extends PureComponent {
+
   render() {
     const finalStats = this.props.game.finalStats;
+    const nBacks = finalStats.game.sequences.map((sequence) => sequence.nBack).reverse();
+    nBacks.pop();
+    const chartData = {
+      labels: new Array(finalStats.game.sequences.length+1).fill(1).map((e, i) => i+1),
+      datasets: [{
+        label: 'N',
+        fill: false,
+        data: nBacks,
+        // backgroundColor: '#770D00',
+        borderColor: ' #770D00',
+        borderWidth: 2,
+        pointRadius: 3,
+        scaleStartValue: 0
+      }]
+      // options: {
+      //   scales: {
+      //     yAxes: [{
+      //       ticks: {
+      //         max: 10,
+      //         min: 0
+      //       }
+      //     }]
+      //   },
+      //   legend: {
+      //     display: true,
+      //     labels: {
+      //       boxWidth: 10,
+      //       padding: 5
+      //     }
+      //   },
+      //   title: 'No. of Sequence Played'
+      // }
+    };
+
     return (finalStats.game.sequences ?
       (
         <div className="game-chart">
-          <RC2 data={finalStats.game.sequences.nBack} type="line" width="500" height="300" redraw />
+          <RC2 data={chartData} type="line" width="500" height="300" redraw="true" />
+          {/* <RC2 data={finalStats.game.sequences.nBack} type="line" width="500" height="300" redraw /> */}
         </div>
       ) : (
         <div className="game-chart"></div>
