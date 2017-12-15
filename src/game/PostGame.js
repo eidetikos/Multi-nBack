0a80ed7410c6131eb58c9e4db1f58e10bd219b06
Zac-L <zachlichens@gmail.com>
2017-12-14 22:39:42 -0800

    Revert "Reset game"

--- src/game/PostGame.js
+++ src/game/PostGame.js
@@ -1,21 +1,10 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
-import { initSequence, newGame, replay } from './actions/actions';
 import Chart from './Chart';
 import './PostGame.css';
 import { ChartStats } from '../styles/style';
 
 class PostGame extends PureComponent {
-
-  handleReset = () => {
-    this.props.replay();
-  }
-
-  handleChangeSettings = () => {
-    this.props.initSequence();
-    this.props.newGame();
-  }
-
   render() {
     const finalStats = this.props.game.finalStats;
     return (finalStats.game ?
@@ -37,8 +26,6 @@ class PostGame extends PureComponent {
                 </ul>
               </fieldset>
             </form>
-            <input type="button" value="Replay Game" onClick={this.handleReset}/>
-            <input type="button" value="Change Settings" onClick={this.handleChangeSettings}/>
           </ChartStats>
         </div>
       ) : (
@@ -51,5 +38,5 @@ export default connect(
   state => ({
     game: state.game
   }),
-  { initSequence, newGame, replay }
+  null
 )(PostGame);
