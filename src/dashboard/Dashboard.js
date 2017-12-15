622a4af8a67342545587c0386d8becfbcde35d71
Zac-L <Zachlichens@gmail.com>
2017-12-14 22:18:50 -0800

    brought in communitystats cal on component mount, also brought in stats to pass down to HighestN, MostSequences, TopScore

---
+++ src/dashboard/Dashboard.js
@@ -0,0 +1,51 @@
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import UserStats from './UserStats';
+import HighestN from './communityStats/HighestN';
+import MostSequences from './communityStats/MostSequences';
+import TopScore from './communityStats/TopScore';
+import { getCommunityStats } from './actions';
+
+import './Dashboard.css';
+
+class Dashboard extends PureComponent {
+
+  componentDidMount() { 
+    this.props.getCommunityStats();
+  }
+  render() {
+    console.log(this.props.stats);
+    const { user, stats } = this.props;
+    return (
+      <main className="dashboard">
+        
+        {user &&
+          <UserStats/>
+        }
+        <section 
+          className="stats community-stats"
+          style={{
+            width: user ? '63%' : '90%',
+            float: user ? 'right' : 'center',
+            margin: user ? '2.5% 2.5% 2.5% 0' : '0 auto'
+          }}
+        >
+          <h2>community stats</h2>
+          <div className="community-leaderboards">
+            <HighestN leaderboard={stats.highestNBackPerDifficulty}/>
+            <MostSequences leaderboard={stats.mostSequencesByUser}/>
+            <TopScore leaderboard={stats.topScoresPerDifficulty}/>
+          </div>
+        </section>
+      </main>
+    );
+  }
+}
+
+export default connect(
+  state => ({
+    user: state.user,
+    stats: state.stats.community
+  }),
+  { getCommunityStats }
+)(Dashboard);
