fbc6e9a77e1db283ccb983a1b16bcbf4a7326917
Zac-L <Zachlichens@gmail.com>
2017-12-15 10:29:03 -0800

    wip: broght in personal stats api call, all data is displaying on page correctly. Just need to style now

--- src/dashboard/UserStats.js
+++ src/dashboard/UserStats.js
@@ -4,15 +4,29 @@ import { connect } from 'react-redux';
 // import './UserStats.css';
 
 class UserStats extends PureComponent {
+  
   render() {
-
-    return (
-
-      <section
-        className="stats user-stats"
-      >
-        <h3>user stats!</h3>
-      </section>
+    const { personal } = this.props;
+    console.log('user stats personal??',personal);
+    
+    return personal ? (
+      <article className="stats user-stats">
+        <h4 className="personal-leaderboard-header">personal stats</h4>
+        <div className="personal-leaderboard-stats">
+          {Object.keys(personal).map(personalStat => (
+            <section key={personalStat} className={`personal-leaderboard-stats-${personalStat}`}>
+              <h5 className="">{personalStat}</h5>
+              <ul>
+                <li>
+                  <span className="">{personal[personalStat]} </span>
+                </li>
+              </ul>
+            </section>
+          ))}
+        </div>
+      </article>
+    ) : (
+      <article className=""></article>
     );
   }
 }
